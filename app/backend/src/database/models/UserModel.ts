1d9e85a7e9855cf357ce431f9b7b2736251f1ea6
Andre Marcos <andrmgc@gmail.com>
2023-01-16 18:31:18 -0300

    retomando...

---
+++ app/backend/src/database/models/UserModel.ts
@@ -0,0 +1,40 @@
+import { Model, STRING, INTEGER } from 'sequelize';
+import db from '.';
+
+export default class Users extends Model {
+  declare id: number;
+  declare username: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+  userModel = new Users();
+}
+
+Users.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: STRING,
+    allowNull: false,
+  },
+  role: {
+    type: STRING,
+    allowNull: false,
+  },
+  email: {
+    type: STRING,
+    allowNull: false,
+  },
+  password: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  timestamps: false,
+  modelName: 'users',
+});
