7506aa0914159961e9e86f171bf7a0ff0e471546
Andre Marcos <andrmgc@gmail.com>
2023-01-19 01:35:24 -0300

    change token to string, add more tests, lines coverage 52%

--- app/backend/src/tests/change.me.test.ts
+++ app/backend/src/tests/change.me.test.ts
@@ -8,7 +8,7 @@ import Users from '../database/models/UserModel';
 
 import { Response } from 'superagent';
 
-import { validUser,noEmail,noPassword,invalidEmail, invalidPassword, token } from './mocks/loginMocks';
+import { validUser,noEmail,noPassword,invalidEmail, invalidPassword, token, user } from './mocks/loginMocks';
 
 chai.use(chaiHttp);
 
@@ -34,4 +34,24 @@ describe('Testes', () => {
     expect(response.status).to.be.equal(400)
     expect(response.body.message).to.be.equal('All fields must be filled')
   });
+  it('login nao deve acontecer quando o email nao eh valido', async () => {
+    const response = await chai.request(app).post('/login').send(invalidEmail);
+    expect(response.status).to.be.equal(401)
+    expect(response.body.message).to.be.equal('Incorrect email or password')
+  });
+  it('login nao deve acontecer quando o password nao eh valido', async () => {
+    const response = await chai.request(app).post('/login').send(invalidPassword);
+    expect(response.status).to.be.equal(401)
+    expect(response.body.message).to.be.equal('Incorrect email or password')
+  });
+  it('deve retornar o token se estiver tudo ok', async () => {
+    const response = await chai.request(app).post('/login').send(user);
+    expect(response.status).to.be.equal(200)
+    expect(response.body).to.haveOwnProperty('token');
+    })
+  it('deve retornar a funcao do usuario se o token estiver ok', async () => {
+    const response = await chai.request(app).get('/login/validate').set('authorization', token);
+    expect(response.status).to.be.equal(200)
+    expect(response.body).to.be.deep.equal({ role: 'admin' })
+  })
 });
--- app/backend/src/tests/mocks/loginMocks.ts
+++ app/backend/src/tests/mocks/loginMocks.ts
@@ -1,6 +1,13 @@
 export const validUser = {
-    email: 'user@user.com',
-    password: 'secret_user',
+    username: 'Admin',
+    role: 'admin',
+    email: 'admin@admin.com',
+    password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW'
+}
+
+export const user = {
+    email: 'admin@admin.com',
+    password: 'secret_admin',
 }
 
 export const noEmail = {
@@ -20,6 +27,5 @@ export const invalidEmail = {
     email: 'nope@nope.com',
     password: 'secret_user',
 }
-export const token = {
-    validToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IkFkbWluIiwiZW1haWwiOiJhZG1pbkBhZG1pbi5jb20iLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE2NzM5NjUzODV9.zs2yyz__mDU3AK-5IVYxydJcMahzMCK0XwHEJ5vJVwE'
-}
+export const token = 
+'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IkFkbWluIiwiZW1haWwiOiJhZG1pbkBhZG1pbi5jb20iLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE2NzM5NjUzODV9.zs2yyz__mDU3AK-5IVYxydJcMahzMCK0XwHEJ5vJVwE'
